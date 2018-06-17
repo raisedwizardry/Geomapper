6024d88b0c0b691c4e4691772aa3ec4d230c4f1f
Jay McCodes <lifebloodasmr@gmail.com>
2018-06-17 13:59:49 -0400

    Add project files.

---
+++ Geomapper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Geomapper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
